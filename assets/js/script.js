cf1075c14c97e2d990a981d231f9c9f496b7b7b8
Fecooo <85518187+Fecooo@users.noreply.github.com>
2023-07-31 01:03:10 +0200

    js folder

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+window.onload = async function(){
+    document.getElementById(`content1`).style.display = 'none';
+    document.getElementById(`content2`).style.display = 'none';
+    await kiirat();
+}
+
+async function kiirat() {
+    const settings  = {
+        method : "GET",
+        headers: {
+            "X-Master-Key" : "$2b$10$iqoX5mAdiyV/GetnOqxw7OsXewvRcUiwcBzWzfUcPinQMsW2y1Fty",
+            "Content-Type": "application/json"
+        }
+    }
+    
+    let response = await fetch("https://api.jsonbin.io/v3/b/647e070cb89b1e2299aa4c0e?meta=false", settings);
+    const data = await response.json();
+    document.getElementById("emberszam").innerHTML = data.members
+    document.getElementById("eselyszam").innerHTML = data.entries
+    document.getElementById("time").innerHTML = data.updated
+
+    const tabla = document.getElementById("main")
+    const tabla2 = document.getElementById("korabbi")
+    
+    for (const key in data.data) {
+        let tr = document.createElement("tr")
+        let td1 = document.createElement("td")
+        let td2 = document.createElement("td")
+
+        td1.innerHTML = data.data[key].name
+        td2.innerHTML = data.data[key].entry
+
+        tr.appendChild(td1)
+        tr.appendChild(td2)
+
+        tabla.appendChild(tr)
+    }
+    
+    for (const key in data.older) {
+        let tr = document.createElement("tr")
+        let td1 = document.createElement("td")
+        let td2 = document.createElement("td")
+        let td3 = document.createElement("td")
+
+        td1.innerHTML = data.older[key].date
+        td2.innerHTML = data.older[key].name
+        td3.innerHTML = data.older[key].entry
+
+        tr.appendChild(td1)
+        tr.appendChild(td2)
+        tr.appendChild(td3)
+
+        tabla2.appendChild(tr)
+    }
+}
+
+function toggleContent(divNumber) {
+    const contentDiv = document.getElementById(`content${divNumber}`);
+    const contentTitle = document.getElementById(`boxtitle${divNumber}`);
+
+    if (contentDiv.style.display === 'none') {
+        contentDiv.style.display = 'block';
+        contentTitle.innerHTML = contentTitle.innerHTML.split(" ▶")[0]
+        contentTitle.innerHTML = contentTitle.innerHTML += " ▼"
+    } else {
+        contentDiv.style.display = 'none';
+        contentTitle.innerHTML = contentTitle.innerHTML.split(" ▼")[0]
+        contentTitle.innerHTML = contentTitle.innerHTML += " ▶"
+    }
+} 
